fb813300191abba2de477fc20a84e358c016260d
heeeeeeybaby <m.fuentes@holymonkey.cl>
2022-09-22 23:35:30 -0300

    Funciona array

--- nuevo-presupuesto.js
+++ nuevo-presupuesto.js
@@ -11,40 +11,40 @@ class Cliente {
         this.camara = camara,
         this.minutaje = minutaje
     }
-    calcularPresupuesto() {
-        return ((this.jornada * valorJornada) + (this.camara * (valorJornada * 0.2)) + (this.minutaje *(valorJornada * (this.minutaje * 0.3)) * this.cantidad)); 
-    }
 }
 
 let arrayClientes = []; 
 
-// Define una clase para el video que cotiza
+// Define ell calculo de presupuesto
+
 const valorJornada = 80000;
 
-const formulario = document.getElementById("formulario");
+function calcularPresupuesto() {
+    let presupuesto = ((Cliente.jornada * valorJornada) + (Cliente.camara * (valorJornada * 0.2)) + (Cliente.minutaje *(valorJornada * (Cliente.minutaje * 0.3)) * Cliente.cantidad)); 
+    return presupuesto; 
+} 
 
+const formulario = document.getElementById("formulario");
 formulario.addEventListener("submit", (e) =>{
 
     e.preventDefault();
 
     const id = Math.ceil(Math.random() * 100); 
-    const nombre = document.getElementById("nombreCliente").value; 
-    const rut = document.getElementById("rutCliente").value; 
-    const empresa = document.getElementById("nombreEmpresa").value;
-    const email = document.getElementById("emailCliente").value;
-    const jornada = document.getElementById("cantJornadas").value; 
-    const camara = document.getElementById("cantCamaras").value;
-    const minutaje = document.getElementById("minutaje").value;
-    const cantidad = document.getElementById("cantVideos").value;
-    const presupuesto = cliente.calcularPresupuesto(); 
-    const cliente = new Cliente(id, nombre, rut, empresa, email, cantidad, jornada, camara, minutaje, presupuesto); 
+    const nombre = document.getElementById("nombreCliente"); 
+    const rut = document.getElementById("rutCliente"); 
+    const empresa = document.getElementById("nombreEmpresa");
+    const email = document.getElementById("emailCliente");
+    const jornada = document.getElementById("cantJornadas"); 
+    const camara = document.getElementById("cantCamaras");
+    const minutaje = document.getElementById("minutaje");
+    const cantidad = document.getElementById("cantVideos");
+    const presupuesto = calcularPresupuesto(); 
+    const cliente = new Cliente(id, nombre.value, rut.value, empresa.value, email.value, cantidad.value, jornada.value, camara.value, minutaje.value); 
 
-    console.log(presupuesto);
     arrayClientes.push(cliente);
     console.log(arrayClientes);
     formulario.reset();
 
-    mostrarInfo(cliente); 
 });
 
-// Define Variables con Información del Usuario
+
